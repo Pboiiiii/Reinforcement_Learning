e4302c68b3aa5e3f364470d896ef5eced7c63039
Concchita <yurrunagaf325@gmail.com>
2025-03-01 13:58:38 -0500

    replay_mem.py

--- replay_mem.py
+++ replay_mem.py
@@ -4,6 +4,7 @@ import random
 class ReplayMemory():
     def __init__(self, maxlen, seed=None):
         self.memory = deque([], maxlen=maxlen)
+        self.maxlen = maxlen
 
         # Optional seed for reproducibility
         if seed is not None:
@@ -17,3 +18,9 @@ class ReplayMemory():
 
     def __len__(self):
         return len(self.memory)
+
+    def state_dict(self):
+        return {"memory": list(self.memory)}
+
+    def load_state_dict(self, state_dict):
+        self.memory = deque(state_dict["memory"], maxlen=self.maxlen)
